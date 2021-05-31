a5d79ebe441aa130fc8d1bf76d0d8e2cddaeaa91
sanjeevyg <yogi.sjv@gmail.com>
2021-05-31 15:23:40 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import { useState, useEffect } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+  const [todos, setTodo] = useState([
+    {title: "First Todo", content: "This is my first todo!"},
+    {title: "Second Todo", content: "This is my second todo!"},
+    {title: "Third Todo", content: "This is my third todo!"}
+  ])
+
+
+  const showTodo = () => todos.map(todo => {
+    return <View style={styles.todoCard}>
+      <Text style={styles.title}>{todo.title}</Text>
+      <Text style={styles.content}>{todo.content}</Text>
     </View>
+  })
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {showTodo()}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  todoCard: {
+    flex: 1, 
+    backgroundColor: "yellow",
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
